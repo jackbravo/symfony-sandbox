e296ad1423cb799e92f9754cab2cca851755cce1
Joaquin Bravo Contreras <jackbravo@gmail.com>
2010-11-27 10:57:50 -0600

    Use validators

--- src/Application/ChiaBundle/Entity/Contact.php
+++ src/Application/ChiaBundle/Entity/Contact.php
@@ -14,6 +14,7 @@ class Contact
 
     /**
      * @var string $name
+     * @validation:NotBlank()
      */
     private $name;
 
@@ -34,6 +35,7 @@ class Contact
 
     /**
      * @var smallint $type
+     * @validation:NotBlank()
      */
     private $type;
 

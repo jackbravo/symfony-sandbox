aeb5172a1ada093512aff9dc17cf4589966c57ca
Joaquin Bravo Contreras <jackbravo@gmail.com>
2010-11-29 13:11:18 -0600

    Added edit action for contacts

--- src/Application/ChiaBundle/Controller/ContactsController.php
+++ src/Application/ChiaBundle/Controller/ContactsController.php
@@ -47,6 +47,38 @@ class ContactsController extends Controller
         return $this->render('ChiaBundle:Contacts:new.twig', array('form' => $form));
     }
 
+    public function editAction($id)
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $companies = $em->createQuery('SELECT c FROM ChiaBundle:Contact c WHERE c.type = 2 ORDER BY c.name')
+            ->getArrayResult();
+        $company_choices = array('' => '---');
+        foreach ($companies as $company) {
+            $company_choices[$company['id']] = $company['name'];
+        }
+
+        $contact = $em->find('Application\ChiaBundle\Entity\Contact', $id);
+
+        $form = new ContactForm('contact', $contact, $this->container->get('validator'), array(
+            'entity_manager' => $em,
+            'company_choices' => $company_choices,
+        ));
+
+        if('POST' === $this->get('request')->getMethod()) {
+            $form->bind($this->get('request')->get('contact'));
+            if($form->isValid()) {
+                $em = $this->container->get('doctrine.orm.entity_manager');
+                $em->persist($contact);
+                $em->flush();
+
+                //$this->container->getSessionService()->setFlash('project_create', array('project' => $project));
+                return $this->redirect($this->generateUrl('contacts'));
+            }
+        }
+
+        return $this->render('ChiaBundle:Contacts:edit.twig', array('form' => $form, 'contact' => $contact));
+    }
+
     public function newCompanyAction()
     {
         $contact = new Contact();
